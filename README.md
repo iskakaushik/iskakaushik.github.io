cc40ff67b7ec80b5dca0779f642ee9c270aaec49
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-08 23:49:14 +0000

    Deploy iskakaushik/iskakaushik.github.io to github.com/iskakaushik/iskakaushik.github.io.git:gh-pages

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kaushik's Blog
+
+Thanks for visiting.
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  watch: true,
+  entry: path.join(__dirname, "webpack", "double-buffer"),
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "assets/js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: [
+          path.resolve(__dirname, "node_modules"),
+          path.resolve(__dirname, "bower_components"),
+        ],
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".json", ".js", ".jsx"],
+  },
+};
